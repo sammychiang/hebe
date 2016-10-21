b6e1301437456e36d95101df1237e83a5cfc4044
sammyjiang <jiangzhifeng2116@gmail.com>
2016-10-21 12:47:27 +0800

    bash

---
+++ hebe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update ; apt-get -y install curl
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs —fix-missing
+apt-get -y install screen
+wget https://gist.githubusercontent.com/joaopizani/2718397/raw/9e2560b77e1e1298ef24be16297d853f9885b20d/.screenrc
+wget https://raw.githubusercontent.com/sammychiang/hebe/master/hebe.js
+npm install request
+apt-get -y install htop
+screen
